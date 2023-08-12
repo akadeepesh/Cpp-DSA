addf6d44833fe1fe633dce0b66c1d4d8d8d8ca9a
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-08-12 15:00:49 +0530

    update

---
+++ Recursion/class_objects.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Car
+{
+public:
+    char name[100];
+    int engine;
+    int price;
+    void print()
+    {
+        cout << name << " " << engine << " " << price << endl;
+    }
+};
+
+int main()
+{
+    Car c;
+    // c.name = "BMW\0";
+    c.engine = 1500;
+    c.price = 15;
+    strcpy(c.name, "BMW");
+    cout << c.engine << " " << c.price << " " << c.name << endl;
+    c.print();
+}
