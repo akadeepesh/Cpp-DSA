feff3f4691efdd201e52f47edc956a1510bbb474
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-08-19 12:46:40 +0530

    hi_string

--- Recursion/hi_string.cpp
+++ Recursion/hi_string.cpp
@@ -2,14 +2,51 @@
 #include <iostream>
 using namespace std;
 
-int hicount(string str, string hi)
+int countHi(string str, int idx)
 {
-    int l;
+    if (idx >= str.length() - 1)
+        return 0;
+
+    if (str.substr(idx, 2) == "hi")
+        return 1 + countHi(str, idx + 2);
+    else
+        return countHi(str, idx + 1);
+}
+
+string removeHi(string str, int idx)
+{
+    if (idx >= str.length() - 1)
+        return str.substr(idx);
+
+    if (str.substr(idx, 2) == "hi")
+        return removeHi(str.substr(0, idx) + str.substr(idx + 2), idx);
+    else
+        return removeHi(str, idx + 1);
+}
+
+string replaceHi(string str, int idx)
+{
+    if (idx >= str.length() - 1)
+        return str.substr(idx);
+
+    if (str.substr(idx, 2) == "hi")
+        return "bye" + replaceHi(str.substr(idx + 2), 0); // Reset idx to 0 after replacement
+    else
+        return str.substr(idx, 1) + replaceHi(str, idx + 1);
 }
 
 int main()
 {
-    string str;
-    cin >> str;
-    cout << hicount(str, "hi") << endl;
+    string input;
+    cin >> input;
+
+    int hiCount = countHi(input, 0);
+    string withoutHi = removeHi(input, 0);
+    string replacedHi = replaceHi(input, 0);
+
+    cout << hiCount << endl;
+    cout << withoutHi << endl;
+    cout << replacedHi << endl;
+
+    return 0;
 }
