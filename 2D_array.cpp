f853da65d75d4e3c5591b27ca8625ed918156563
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-08-19 15:09:59 +0530

    commit

--- 2D_array.cpp
+++ 2D_array.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main()
 {
-    //all initialization are correct
+    // all initialization are correct
     int arr[5][3] = {{1, 2, 3}, {4, 5, 6}, {6, 7, 9}, {10, 11, 12}, {13, 14, 15}};
     // int arr[][3] = {{1, 2, 3}, {4, 5, 6}, {6, 7, 9}, {10, 11, 12}, {13, 14, 15}};
-    
+
     int buckets = sizeof(arr) / sizeof(arr[0][0]);
     int rows = sizeof(arr) / sizeof(arr[0]);
     int col = sizeof(arr[0]) / sizeof(arr[0][0]);
 
-    //array print-->
+    // array print-->
 
     for (int i = 0; i < rows; i++)
     {
